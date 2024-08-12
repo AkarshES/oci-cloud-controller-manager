edecaadbf7943facba458b12505d91a6746efa2f
l-technicore <lalit.s.singh@oracle.com>
2024-08-12 18:45:45 +0530

    env file

--- run_dind.sh
+++ run_dind.sh
@@ -6,7 +6,9 @@ env_file=$(mktemp)
 unset OCI_RESOURCE_PRINCIPAL_PRIVATE_PEM
 # env | grep -v XDG_SESSION_ID | grep -v TEAMCITY_BUILD_PROPERTIES_FILE | grep -v TMPDIR | grep -v JDK_18_x64 | grep -v TEAMCITY_CAPTURE_ENV | grep -v "^USER" | grep -v TEMP | grep -v JDK_18 | grep -v JRE_HOME | grep -v PATH | grep -v PWD | grep -v JAVA_HOME | grep -v LANG | grep -v SHLVL | grep -v HOME | grep -v JDK_HOME | grep -v XDG_RUNTIME_DIR > "$env_file"
 
-sudo env > "${env_file}"
+sudo chmod 777 "${env_file}"
+env > "${env_file}"
+env
 
 #sudo >.env_file
 #for var in $(compgen -v | grep -Ev '^(BASH)'); do
