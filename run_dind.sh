849c2e9708d36bfa8cf5b8342c8a48d0cb4e558e
l-technicore <lalit.s.singh@oracle.com>
2024-08-13 15:04:25 +0530

    more set env

--- run_dind.sh
+++ run_dind.sh
@@ -2,13 +2,13 @@
 
 set -x -v
 
-#env_file=$(mktemp)
-#unset OCI_RESOURCE_PRINCIPAL_PRIVATE_PEM
-#unset BB_KEY
+env_file=$(mktemp)
+unset OCI_RESOURCE_PRINCIPAL_PRIVATE_PEM
+unset BB_KEY
 #unset PUB_SSHKEY
 #env | grep -v XDG_SESSION_ID | grep -v TEAMCITY_BUILD_PROPERTIES_FILE | grep -v TMPDIR | grep -v JDK_18_x64 | grep -v TEAMCITY_CAPTURE_ENV | grep -v "^USER" | grep -v TEMP | grep -v JDK_18 | grep -v JRE_HOME | grep -v PATH | grep -v PWD | grep -v JAVA_HOME | grep -v LANG | grep -v SHLVL | grep -v HOME | grep -v JDK_HOME | grep -v XDG_RUNTIME_DIR > "$env_file"
 
-#env > "${env_file}"
+env > "${env_file}"
 cat "${env_file}"
 
 #touch "${env_file}"
@@ -16,13 +16,13 @@ cat "${env_file}"
 #env
 #cat "${env_file}"
 
-#>.env_file
-#for var in $(compgen -v | grep -Ev '^(BASH)'); do
-#    var_fixed=$(printf "%s" "${!var}" | tr -d '\n' )
-#    echo "$var='${var_fixed}'" >>.env_file
-#done
+>.env_file
+for var in $(compgen -v | grep -Ev '^(BASH)'); do
+    var_fixed=$(printf "%s" "${!var}" | tr -d '\n' )
+    echo "$var='${var_fixed}'" >>.env_file
+done
 
-#$(cat .env_file)
+$(cat .env_file)
 
 export E2E_TEST_BASE_IMAGE=${E2E_TEST_BASE_IMAGE//artifactory.oci.oraclecorp.com/pipelines.artifactory.us-phoenix-1.oci.oracleiaas.com}
 
