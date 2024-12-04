e911964120a366c6de19f08ca16b0b087f4b3a96
ypgohoka <yashwant.p.gohokar@oracle.com>
2024-12-04 14:19:43 +0530

    OKE-32428

---
+++ infra-release-validator-pop/run-command/start/00-run.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+
+exec &> >(tee -a "${ODO_APPLICATION_ROOT}/var/start.log")
+
+echo "Starting image release validation"
+
+if [[ -z "$ODO_APPLICATION_ROOT" ]]; then
+  echo "No ODO_APPLICATION_ROOT defined, cannot continue"
+  exit 1
+fi
+
+JSON_FILE="${ODO_APPLICATION_ROOT}/image_versions.json"
+
+COMPARTMENT_OCID=$STEWARD_TENANCY_OCID
+
+fetch_repository_tags() {
+    local repo_name=$1
+    oci artifacts container image list \
+        --compartment-id "$COMPARTMENT_OCID" \
+        --region "$REGION" \
+        --repository-name "$repo_name" \
+        --auth instance_principal \
+        --query 'data.items[*]."display-name"' \
+        --output json | jq -r '.[]' | awk -F':' '{print $2}'
+}
+
+jq -r '.images[] | keys[]' "$JSON_FILE" | sort -u | while read -r repo_name; do
+    echo "Checking repository: $repo_name"
+
+    repo_tags=$(fetch_repository_tags "$repo_name")
+
+    expected_tags=$(jq -r --arg repo "$repo_name" '.images[][$repo] // empty' "$JSON_FILE")
+
+    echo "Tags expected: $expected_tags"
+    for tag in $expected_tags; do
+        if echo "$repo_tags" | grep -q "^$tag$"; then
+            echo "  Tag found: $tag"
+        else
+            echo "  Tag missing: $tag"
+            exit 1
+        fi
+    done
+    echo
+done
+
+echo "All images found in OCIR."
