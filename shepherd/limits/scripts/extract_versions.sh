10829c51d61728f547834554f34a0d132dc7b165
ypgohoka <yashwant.p.gohokar@oracle.com>
2024-12-04 14:19:43 +0530

    OKE-32428 - Validate presence of images before applying them in infra release

--- shepherd/limits/scripts/extract_versions.sh
+++ shepherd/limits/scripts/extract_versions.sh
@@ -1,26 +1,4 @@
 #!/bin/bash
-set -e
-if [[ "$#" -ne 1 ]]; then
-    >&2 echo "Usage: ./gen_images_tfvars.sh <path_to_wheelbarrow_manifest>"
-    exit
-fi
-manifest=$1
-
-echo "{\"images\": ["
-
-awk ' \
-  BEGIN { SEP=""; FS=",";} \
-  /.\/*/{ \
-    gsub(/^[ \t]+|[ \t]+$/, "", $1); \
-    gsub(/^[ \t]+|[ \t]+$/, "", $2); \
-    gsub("\\\.", "_DOT_", $2); \
-    printf "%s  {\"name\": \"%s__%s\", \"location\": \"%s\"}", SEP, $1, $2, $1; \
-    SEP=",\n"; \
-  } \
-  END {printf "\n"}' ${manifest}
-
-echo "]}"
-
 
 if [ "$#" -ne 1 ]; then
   echo "Usage: $0 <path-to-wheelbarrow-manifest>"
---
+++ shepherd/limits/shared_modules/odo_configuration/main.tf
@@ -0,0 +1,51 @@
+data "odo_pools" "odo-pool" {
+  ad                      = var.physical_ad1
+  pool_name_regex         = var.pool_name_regex
+}
+
+resource "odo_application" "applications" {
+  ad                      = var.physical_ad1
+  alias                   = var.application_alias
+  compartment_ocid        = var.compartment_id
+  type                    = var.stage == "prod" ? "PRODUCTION" : "NON_PRODUCTION"
+  default_artifact_source = "DEFAULT"
+  artifact_set_identifier = var.artifact_set_identifier
+  agent                   = "HOSTAGENT_V2"
+  pools                   = data.odo_pools.odo-pool.pools[*].resource_id
+
+  config {
+    deployments {
+      min_nodes_in_service_percent = 0
+      parallelism_type = "HOSTS"
+      parallelism = 1
+      deploy_sequentially = true
+      fault_domain_deploy_sequentially = true
+      ttl_seconds_pull_image           = 240
+      ttl_seconds_start_instance       = 240
+      ttl_seconds_stop_instance        = 300
+      ttl_seconds_validation           = 240
+    }
+
+    environment_variables {
+      name = "STEWARD_TENANCY_OCID"
+      value = lookup(lookup(local.steward_tenancy_info, local.execution_target.region.realm, {}), "ocid", null)
+    }
+
+    environment_variables {
+      name = "REGION"
+      value = local.execution_target.region.name
+    }
+
+    dynamic environment_variables {
+      for_each = {for variable in var.env_vars: variable.name => variable.value}
+      content {
+        name  = environment_variables.key
+        value = environment_variables.value
+      }
+    }
+  }
+
+  lifecycle {
+    ignore_changes = [ default_artifact_source ]
+  }
+}
