1cef3827a76fe2bbdd3157fdfdc1b16803034284
unepatel <uneet.patel@oracle.com>
2025-02-26 15:08:20 +0530

    OKE-35257 Change fvd rpm package name and path to integrate with node packages

--- rpm/fvd_rpm.mk
+++ rpm/fvd_rpm.mk
@@ -1,17 +1,17 @@
-VERSION          ?= $(shell cat ocibuild.conf | grep ccmVersion: | cut -d' ' -f2 | sed 's/"//g' )
-NAME             ?= oci
-BLD_ARCH         ?= x86
-FVD_BINARY_PATH  ?= ""
-WORK_DIR 		 ?= ~/sparta/input
-RPM_INSTALL_PATH ?= "/etc/libexec/kubernetes/kubelet-plugins/volume/exec/oracle~oci/"
-FVD_BINARY_NAME ?= "oci-flexvolume-driver"
-RPM_VERSION ?= $(subst -,_,$(VERSION))
-RELEASE ?= 1
+VERSION						?= $(shell cat ocibuild.conf | grep k8sVersionWithoutV: | cut -d' ' -f2 | sed 's/"//g' )
+FVD_INSTALL_NAME			?= oci
+BLD_ARCH					?= x86
+FVD_BINARY_PATH				?= ""
+WORK_DIR					?= ~/sparta/input
+RPM_INSTALL_PATH			?= "/etc/libexec/kubernetes/kubelet-plugins/volume/exec/oracle~oci/"
+INPUT_FVD_BINARY_NAME		?= "oci-flexvolume-driver"
+RPM_VERSION					?= $(subst -,_,$(VERSION))
+RELEASE						?= 1
 
 .PHONY: PACKAGE_TARGET
-PKG_TARGET := $(WORK_DIR)/rpmbuild/RPMS/$(BLD_ARCH)/$(NAME)-$(RPM_VERSION).$(BLD_ARCH).rpm
-PKG_SOURCE := $(WORK_DIR)/rpmbuild/SOURCES/$(NAME)-$(RPM_VERSION).tar.gz
-PKG_SPEC   := $(WORK_DIR)/rpmbuild/SPECS/$(NAME).spec
+PKG_TARGET := $(WORK_DIR)/rpmbuild/RPMS/$(BLD_ARCH)/$(FVD_INSTALL_NAME)-$(RPM_VERSION).$(BLD_ARCH).rpm
+PKG_SOURCE := $(WORK_DIR)/rpmbuild/SOURCES/$(FVD_INSTALL_NAME)-$(RPM_VERSION).tar.gz
+PKG_SPEC   := $(WORK_DIR)/rpmbuild/SPECS/$(FVD_INSTALL_NAME).spec
 
 .DEFAULTTARGET: rpm
 
@@ -23,17 +23,17 @@ setup:
 rpm: $(PKG_TARGET)
 
 $(PKG_TARGET): $(PKG_SPEC) $(PKG_SOURCE)
-	rpmbuild -bb \
-		--define "name $(NAME)" \
+	rpmbuild -v -bb \
 		--define "_version $(RPM_VERSION)" \
 		--define "_topdir $(WORK_DIR)/rpmbuild" \
 		--define "_flexvolume_install_path $(RPM_INSTALL_PATH)" \
+		--define "_flexvolume_install_name $(FVD_INSTALL_NAME)" \
 		--define "_release $(RELEASE)" $(WORK_DIR)/rpmbuild/SPECS/fvd.spec
 
 $(PKG_SOURCE): $(FVD_BINARY_PATH) | rpmbuild
 	mkdir -p $(dir $@)
-	cp $(FVD_BINARY_PATH)/$(FVD_BINARY_NAME) $(FVD_BINARY_PATH)/$(NAME)
-	tar -czvf $@ -C $(FVD_BINARY_PATH) $(NAME)
+	cp $(FVD_BINARY_PATH)/$(INPUT_FVD_BINARY_NAME) $(FVD_BINARY_PATH)/$(FVD_INSTALL_NAME)
+	tar -czvf $@ -C $(FVD_BINARY_PATH) $(FVD_INSTALL_NAME)
 
 
 $(PKG_SPEC): rpmbuild
