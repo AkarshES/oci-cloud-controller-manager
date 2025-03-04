0eabe0296dba03b86ce3a042a0bdae2ee23b2f80
ypgohoka <yashwant.p.gohokar@oracle.com>
2025-03-04 10:09:31 +0530

    test

--- shepherd/limits/infrastructure/generic_region/infra-validation/default_values.tf
+++ shepherd/limits/infrastructure/generic_region/infra-validation/default_values.tf
@@ -7,13 +7,14 @@ locals {
         "default" : "oke-multiarch-1.23-526d1e6-171@sha256:85235e1fa24c41e5fb158346e3339fc680dcdce791735bfca25c7755a479e4c8",
         "v1.23" : "oke-multiarch-1.23-526d1e6-171@sha256:85235e1fa24c41e5fb158346e3339fc680dcdce791735bfca25c7755a479e4c8",
         "v1.24" : "v1.24-32be19ef595-4@sha256:3eda1610412ce5a3f6009b1d1a9219b3fdcc59009a8e3077a83f2b82142a586e",
-        "v1.25" : "v1.25-d0b59914251-3@sha256:ffa44cb1e6bc5793859a1ebf7762bcd1731ec0dea426034c44bc77ffb12ece48",
+        "v1.25" : "v1.25-d0b59914251-38@sha256:ffa44cb1e6bc5793859a1ebf7762bcd1731ec0dea426034c44bc77ffb12ece48",
         "v1.26" : "v1.26-8744a6c9ccd-42@sha256:bc20c825c5e3b5f40b56467e3b931597a6edef41cd0ab0cb20524b4cd8e603a0",
         "v1.27" : "v1.27-7e9bf7a9189-52@sha256:fd510337e52ef609ffb86953ef080b3f2fa1a19a0647f65dfbd2c1dfbda0df7a",
         "v1.28" : "v1.28-cb1635cc6c7-80@sha256:d9e51c8c78b3ef040e5739d6ac079d6587bb4b03d8b65579a877b4ec84c4f219",
         "v1.29" : "v1.29-13ef016091f-80@sha256:2c7bf660d548975c3c7cf079153eec6e716a9399e6eb01cde5f56102125ef40b",
         "v1.30" : "v1.30-424c53e6898-73@sha256:0fde8115356b9654282bf361c7ae55aa9b95c510c34775bec501264bca999648",
-        "v1.31" : "v1.31-46a28c37e3d-32@sha256:64472a97837f58f3f33489077923b0babf8802453f16b195b260b559fbd52b6d"
+        "v1.31" : "v1.31-46a28c37e3d-32@sha256:64472a97837f58f3f33489077923b0babf8802453f16b195b260b559fbd52b6d",
+        "v1.32" : "v1.32-2eb6e737df9-2@sha256:26c7eb0833c08c69508beadfd0b135d7e312cc3dcceab6e34e8ac6030753ef46"
       }
     }
   }
@@ -28,9 +29,10 @@ locals {
         "v1.26" : "v1.26-d5c95b4f813-25@sha256:d401f00fc5f6d2710f916b44450c5f4264673f1403a4aabeba98393e318fcec1",
         "v1.27" : "v1.27-bff7091ad1d-51@sha256:10555f13db26cbcb35dcd684be88db9897e01ad25bdcd7d65e9cb4bd1c5f386f",
         "v1.28" : "v1.28-cb1635cc6c7-80@sha256:d9e51c8c78b3ef040e5739d6ac079d6587bb4b03d8b65579a877b4ec84c4f219",
-        "v1.29" : "v1.29-13ef016091f-80@sha256:2c7bf660d548975c3c7cf079153eec6e716a9399e6eb01cde5f56102125ef40b",
-        "v1.30" : "v1.30-424c53e6898-73@sha256:0fde8115356b9654282bf361c7ae55aa9b95c510c34775bec501264bca999648",
-        "v1.31" : "v1.31-46a28c37e3d-32@sha256:64472a97837f58f3f33489077923b0babf8802453f16b195b260b559fbd52b6d"
+        "v1.29" : "v1.29-9fb67276896-88@sha256:bf72989625783bde02c57668bb45a0b95a4e162e22c33fcc379296544bf24e49",
+        "v1.30" : "v1.30-355fd0c359b-81@sha256:8c5a4e0ba89ef4c6b166e66837509bd633d9c051797a184a832cb5fe50121e21",
+        "v1.31" : "v1.31-ebe76e102e6-39@sha256:64b8ea55494fe1361b6aae7c5256ef9aba8d15837c1edf9f8cd2e07eb89984dd",
+        "v1.32" : "v1.32-01ab1cd7cc9-5@sha256:e7992d6fbf4c61ec8de8b539528cd63b9f3d99948d09ffdafa39fd59949a4f70"
       }
     }
   }
@@ -102,10 +104,7 @@ locals {
     }
 
     lustre-csi-driver-enabled = {
-      "dev.oc1" = {
-        "all": "true"
-      }
-      "integ.oc1" = {
+      "default" = {
         "all": "true"
       }
     }
@@ -127,7 +126,6 @@ locals {
             "v1.20" : "oke-multiarch-1.19-64ab664-255@sha256:c0b0b665735d3288d0f8991c792c51aa00f9aaa031e2ffdd5ecca0238c03f28b",
             "v1.21" : "oke-multiarch-1.19-64ab664-255@sha256:c0b0b665735d3288d0f8991c792c51aa00f9aaa031e2ffdd5ecca0238c03f28b",
             "v1.22" : "oke-multiarch-1.22-9893434-269@sha256:ceba7b8788c84d494113c862cd03dce2cc2c7b52c451ebeaa6eee88a97a4d8db",
-            "v1.32" : "v1.32-fef3de1bc29-34@sha256:6c3464154b74dcf1d054ed7e52225edf35966a3d9f7e18f8b8e61bfe92a787e2",
           }
         ))
         "iad" : jsonencode(merge(local.ccm_default_mapping.default.all,
@@ -140,7 +138,6 @@ locals {
             "v1.20" : "oke-multiarch-1.19-64ab664-255@sha256:c0b0b665735d3288d0f8991c792c51aa00f9aaa031e2ffdd5ecca0238c03f28b",
             "v1.21" : "oke-multiarch-1.19-64ab664-255@sha256:c0b0b665735d3288d0f8991c792c51aa00f9aaa031e2ffdd5ecca0238c03f28b",
             "v1.22" : "oke-multiarch-1.22-9893434-269@sha256:ceba7b8788c84d494113c862cd03dce2cc2c7b52c451ebeaa6eee88a97a4d8db",
-            "v1.32" : "v1.32-fef3de1bc29-34@sha256:6c3464154b74dcf1d054ed7e52225edf35966a3d9f7e18f8b8e61bfe92a787e2",
           }
         ))
         "phx" : jsonencode(merge(local.ccm_default_mapping.default.all,
@@ -153,7 +150,6 @@ locals {
             "v1.20" : "oke-multiarch-1.19-64ab664-255@sha256:c0b0b665735d3288d0f8991c792c51aa00f9aaa031e2ffdd5ecca0238c03f28b",
             "v1.21" : "oke-multiarch-1.19-64ab664-255@sha256:c0b0b665735d3288d0f8991c792c51aa00f9aaa031e2ffdd5ecca0238c03f28b",
             "v1.22" : "oke-multiarch-1.22-9893434-269@sha256:ceba7b8788c84d494113c862cd03dce2cc2c7b52c451ebeaa6eee88a97a4d8db",
-            "v1.32" : "v1.32-fef3de1bc29-34@sha256:6c3464154b74dcf1d054ed7e52225edf35966a3d9f7e18f8b8e61bfe92a787e2",
           }
         ))
         "fra" : jsonencode(merge(local.ccm_default_mapping.default.all,
@@ -166,7 +162,6 @@ locals {
             "v1.20" : "oke-multiarch-1.19-64ab664-255@sha256:c0b0b665735d3288d0f8991c792c51aa00f9aaa031e2ffdd5ecca0238c03f28b",
             "v1.21" : "oke-multiarch-1.19-64ab664-255@sha256:c0b0b665735d3288d0f8991c792c51aa00f9aaa031e2ffdd5ecca0238c03f28b",
             "v1.22" : "oke-multiarch-1.22-9893434-269@sha256:ceba7b8788c84d494113c862cd03dce2cc2c7b52c451ebeaa6eee88a97a4d8db",
-            "v1.32" : "v1.32-fef3de1bc29-34@sha256:6c3464154b74dcf1d054ed7e52225edf35966a3d9f7e18f8b8e61bfe92a787e2",
           }
         ))
       }
@@ -182,7 +177,6 @@ locals {
             "v1.20" : "oke-multiarch-1.19-64ab664-255@sha256:c0b0b665735d3288d0f8991c792c51aa00f9aaa031e2ffdd5ecca0238c03f28b",
             "v1.21" : "oke-multiarch-1.19-64ab664-255@sha256:c0b0b665735d3288d0f8991c792c51aa00f9aaa031e2ffdd5ecca0238c03f28b",
             "v1.22" : "oke-multiarch-1.22-9893434-269@sha256:ceba7b8788c84d494113c862cd03dce2cc2c7b52c451ebeaa6eee88a97a4d8db",
-            "v1.32" : "v1.32-fef3de1bc29-34@sha256:6c3464154b74dcf1d054ed7e52225edf35966a3d9f7e18f8b8e61bfe92a787e2",
           }
         ))
         "iad" : jsonencode(merge(local.ccm_default_mapping.default.all,
@@ -195,7 +189,6 @@ locals {
             "v1.20" : "oke-multiarch-1.19-64ab664-255@sha256:c0b0b665735d3288d0f8991c792c51aa00f9aaa031e2ffdd5ecca0238c03f28b",
             "v1.21" : "oke-multiarch-1.19-64ab664-255@sha256:c0b0b665735d3288d0f8991c792c51aa00f9aaa031e2ffdd5ecca0238c03f28b",
             "v1.22" : "oke-multiarch-1.22-9893434-269@sha256:ceba7b8788c84d494113c862cd03dce2cc2c7b52c451ebeaa6eee88a97a4d8db",
-            "v1.32" : "v1.32-fef3de1bc29-34@sha256:6c3464154b74dcf1d054ed7e52225edf35966a3d9f7e18f8b8e61bfe92a787e2",
           }
         ))
         "phx" : jsonencode(merge(local.ccm_default_mapping.default.all,
@@ -208,7 +201,6 @@ locals {
             "v1.20" : "oke-multiarch-1.19-64ab664-255@sha256:c0b0b665735d3288d0f8991c792c51aa00f9aaa031e2ffdd5ecca0238c03f28b",
             "v1.21" : "oke-multiarch-1.19-64ab664-255@sha256:c0b0b665735d3288d0f8991c792c51aa00f9aaa031e2ffdd5ecca0238c03f28b",
             "v1.22" : "oke-multiarch-1.22-9893434-269@sha256:ceba7b8788c84d494113c862cd03dce2cc2c7b52c451ebeaa6eee88a97a4d8db",
-            "v1.32" : "v1.32-fef3de1bc29-34@sha256:6c3464154b74dcf1d054ed7e52225edf35966a3d9f7e18f8b8e61bfe92a787e2",
           }
         ))
         "fra" : jsonencode(merge(local.ccm_default_mapping.default.all,
@@ -221,7 +213,6 @@ locals {
             "v1.20" : "oke-multiarch-1.19-64ab664-255@sha256:c0b0b665735d3288d0f8991c792c51aa00f9aaa031e2ffdd5ecca0238c03f28b",
             "v1.21" : "oke-multiarch-1.19-64ab664-255@sha256:c0b0b665735d3288d0f8991c792c51aa00f9aaa031e2ffdd5ecca0238c03f28b",
             "v1.22" : "oke-multiarch-1.22-9893434-269@sha256:ceba7b8788c84d494113c862cd03dce2cc2c7b52c451ebeaa6eee88a97a4d8db",
-            "v1.32" : "v1.32-fef3de1bc29-34@sha256:6c3464154b74dcf1d054ed7e52225edf35966a3d9f7e18f8b8e61bfe92a787e2",
           }
         ))
       }
@@ -817,28 +808,28 @@ locals {
     }
   }
   global_default_values_by_property = {
-  for property_name, property_value in local.global_default_values : property_name => merge(
-    lookup(property_value, "default", {}),
-    lookup(property_value, var.env, {}),
-    lookup(property_value, "${var.env}.${var.realm}", {})
-  )
+    for property_name, property_value in local.global_default_values : property_name => merge(
+      lookup(property_value, "default", {}),
+      lookup(property_value, var.env, {}),
+      lookup(property_value, "${var.env}.${var.realm}", {})
+    )
   }
 
   global_default_values_list = flatten([
-  for property_name, property_value in local.global_default_values_by_property : [
-    {
-      ad     = lookup(property_value, "ad", "all")
-      group  = var.spectre_group_name
-      name   = replace(property_name, "_", "-")
-      region = var.execution_target.additional_locals.limits_region
-      value  = lookup(property_value, var.execution_target.additional_locals.limits_region, lookup(property_value, "all", ""))
-      min    = lookup(property_value, "min", null)
-      max    = lookup(property_value, "max", null)
-    }
-  ] if length(property_value) > 0
+    for property_name, property_value in local.global_default_values_by_property : [
+      {
+        ad     = lookup(property_value, "ad", "all")
+        group  = var.spectre_group_name
+        name   = replace(property_name, "_", "-")
+        region = var.execution_target.additional_locals.limits_region
+        value  = lookup(property_value, var.execution_target.additional_locals.limits_region, lookup(property_value, "all", ""))
+        min    = lookup(property_value, "min", null)
+        max    = lookup(property_value, "max", null)
+      }
+    ] if length(property_value) > 0
   ])
 
   global_default_values_map = {
-  for property in local.global_default_values_list : "${property.group}/${property.name}/${property.region}/${property.ad}" => property
+    for property in local.global_default_values_list : "${property.group}/${property.name}/${property.region}/${property.ad}" => property
   }
 }
