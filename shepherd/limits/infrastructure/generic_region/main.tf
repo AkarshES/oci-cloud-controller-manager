3a5f47ec121d95e177572cf662203e9050fe0af8
ypgohoka <yashwant.p.gohokar@oracle.com>
2025-03-05 18:35:10 +0530

    script change to validate digest also in infra release

--- shepherd/limits/infrastructure/generic_region/main.tf
+++ shepherd/limits/infrastructure/generic_region/main.tf
@@ -91,20 +91,10 @@ locals {
   regional_values = [for mapping in module.validation_module.regional_values: mapping.value if mapping.region == local.execution_target.additional_locals.limits_region]
   override_values = [for mapping in module.validation_module.override_values: mapping.value if mapping.region == local.execution_target.additional_locals.limits_region]
 
-  raw_regional_image_list = [for v in local.regional_values : regexall("\"([^\"]+?)@sha256", v)]
-  raw_override_image_list = [for v in local.override_values : regexall("\"([^\"]+?)@sha256", v)]
+  raw_regional_image_list = [for v in local.regional_values : regexall("\"[^\"]+@sha256:[^\"]+\"", v)]
+  raw_override_image_list = [for v in local.override_values : regexall("\"[^\"]+@sha256:[^\"]+\"", v)]
 
-  regional_image_list = tolist(toset(flatten(local.raw_regional_image_list)))
-  overrides_image_list = tolist(toset(flatten(local.raw_override_image_list)))
-
-  regional_list_mid_index = floor(length(local.regional_image_list) / 2)
-  overrides_list_mid_index = floor(length(local.overrides_image_list) / 2)
-
-  regional_image_list_first_half = length(local.regional_image_list) > 0 ? slice(local.regional_image_list, 0, local.regional_list_mid_index) : []
-  regional_image_list_second_half = length(local.regional_image_list) > 0 ? slice(local.regional_image_list, local.regional_list_mid_index, length(local.regional_image_list) - 1) : []
-
-  overrides_image_list_first_half = length(local.overrides_image_list) > 0 ? slice(local.overrides_image_list, 0, local.overrides_list_mid_index) : []
-  overrides_image_list_second_half = length(local.overrides_image_list) > 0 ? slice(local.overrides_image_list, local.overrides_list_mid_index, length(local.overrides_image_list) - 1) : []
+  combined_images = distinct(flatten(concat(raw_regional_image_list, raw_override_image_list)))
 }
 
 data "odo_applications" "infra-release-validator-ccm-csi" {
@@ -154,26 +144,16 @@ module "odo_configuration_ccm_csi_infra" {
   physical_ad1            = module.ad_map.physical_ad1.name
   application_alias = "infra-release-validator-ccm-csi-${local.execution_target.additional_locals.env}"
   env_vars = [
+    for index, image in local.combined_images :
     {
-      name = "REGIONAL_IMAGE_LIST_1"
-      value = join(",", local.regional_image_list_first_half)
-    },
-    {
-      name = "REGIONAL_IMAGE_LIST_2"
-      value = join(",", local.regional_image_list_second_half)
-    },
-    {
-      name = "OVERRIDES_IMAGE_LIST_1"
-      value = join(",", local.overrides_image_list_first_half)
-    },
-    {
-      name = "OVERRIDES_IMAGE_LIST_2"
-      value = join(",", local.overrides_image_list_second_half)
+      name  = format("image_%d", index + 1)
+      value = image
     }
   ]
 }
 
 module "odo_deployment_ccm_csi_infra" {
+  count = var.cpo-image-validation-enabled ? 1 : 0
   source = "./odo_deployment"
 
   artifact_version = {
@@ -181,12 +161,13 @@ module "odo_deployment_ccm_csi_infra" {
     type = "pop"
     version = local.pop_version
   }
-  apps             = [
-    for i in range(length(data.odo_applications.infra-release-validator-ccm-csi)) : {
+  apps = length(data.odo_applications.infra-release-validator-ccm-csi.applications) > 0 ? [
+    for app in data.odo_applications.infra-release-validator-ccm-csi.applications :
+    {
       ad    = module.ad_map.physical_ad1.name,
-      alias = lookup(data.odo_applications.infra-release-validator-ccm-csi[i].applications[0], "alias", null)
+      alias = lookup(app, "alias", null)
     }
-  ]
+  ] : []
   depends_on = [module.odo_configuration_ccm_csi_infra]
 }
 
