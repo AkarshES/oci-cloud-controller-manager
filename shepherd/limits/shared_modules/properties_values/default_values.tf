3b9cbdb88f52de946969307459d5aeaffcbf7591
ypgohoka <yashwant.p.gohokar@oracle.com>
2025-04-08 08:20:16 +0530

    Changes in validator - ignore missing images lesser than v1.28

--- shepherd/limits/shared_modules/properties_values/default_values.tf
+++ shepherd/limits/shared_modules/properties_values/default_values.tf
@@ -1,6 +1,6 @@
 locals {
   // Update the pop version corresponding to the pop build for app release
-  pop_version = "42adb2ce10d_25"
+  pop_version = "9a60ea9a641_52"
 
   // Update the ccm image sha value here for updating CCM versions for respective k8s versions across all realms
   ccm_default_mapping = {
