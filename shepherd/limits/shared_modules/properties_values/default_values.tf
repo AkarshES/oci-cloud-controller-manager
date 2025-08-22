292c264034b8b5a80b6a3b108a776ceccf9e5e9b
Yashas G <yashas.g.g@oracle.com>
2025-08-22 07:25:27 +0530

    Updating pop version to latest

--- shepherd/limits/shared_modules/properties_values/default_values.tf
+++ shepherd/limits/shared_modules/properties_values/default_values.tf
@@ -1,6 +1,6 @@
 locals {
   // Update the pop version corresponding to the pop build for app release
-  pop_version = "0833a431093_111"
+  pop_version = "a15758ce7b7_120"
 
   // Update the ccm image sha value here for updating CCM versions for respective k8s versions across all realms
   ccm_default_mapping = {
